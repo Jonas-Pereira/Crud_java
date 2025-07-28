c9240acc7a0c2ea797dcd53dcf1742547eabdea9
Jonas Pereira <jp_contabilidade@hotmail.com>
2025-07-27 21:51:29 -0300

    comitar
    comitar

--- src/main/java/controller/Funcionario_controller.java
+++ src/main/java/controller/Funcionario_controller.java
@@ -10,58 +10,34 @@ public class Funcionario_controller {
     private String endereco_funcionario;
     private String email;
 
-    /**
-     * @return the id
-     */
-    public int getId() {
+     public int getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
-    public void setId(int id) {
+       public void setId(int id) {
         this.id = id;
     }
 
-    /**
-     * @return the nome_funcionario
-     */
     public String getNome_funcionario() {
         return nome_funcionario;
     }
 
-    /**
-     * @param nome_funcionario the nome_funcionario to set
-     */
     public void setNome_funcionario(String nome_funcionario) {
         this.nome_funcionario = nome_funcionario;
     }
 
-    /**
-     * @return the endereco_funcionario
-     */
     public String getEndereco_funcionario() {
         return endereco_funcionario;
     }
 
-    /**
-     * @param endereco_funcionario the endereco_funcionario to set
-     */
     public void setEndereco_funcionario(String endereco_funcionario) {
         this.endereco_funcionario = endereco_funcionario;
     }
 
-    /**
-     * @return the email
-     */
     public String getEmail() {
         return email;
     }
 
-    /**
-     * @param email the email to set
-     */
     public void setEmail(String email) {
         this.email = email;
     }
